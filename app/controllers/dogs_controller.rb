f458d8bccaf634438d302eadcbec3e2c811c8f86
richietam <richardtam.ny@gmail.com>
2019-03-25 15:48:50 -0400

    Dog class routes and controller feature complete

--- app/controllers/dogs_controller.rb
+++ app/controllers/dogs_controller.rb
@@ -1,2 +1,57 @@
 class DogsController < ApplicationController
+
+  def new
+    users
+    @dog = Dog.new
+  end
+
+  def create
+    users
+    @dog = Dog.create(dog_params)
+      if @dog.valid?
+        redirect_to dog_path(@dog)
+      else
+        flash[:errors] = @dog.errors.full_messages
+        render :new
+      end
+    end
+
+  def show
+    find_dog
+    render :show
+  end
+
+  def edit
+    users
+    find_dog
+    render :edit
+  end
+
+  def update
+    users
+    find_dog
+    @dog.update(dog_params)
+    if @dog.valid?
+      redirect_to dog_pat(@dog)
+    else
+      flash[:errors] = @dog.errors.full_messages
+    end
+  end
+
+  private
+
+  def find_dog
+    @dog = Dog.find(params[:id])
+  end
+
+  def dog_params
+    params.require(:dog).permit(:name, :breed, :age, :hypo_allergenic, :care_info, :user_id)
+  end
+
+  def users
+    @users = User.all
+  end
+
+
+
 end
