5e8dbc070aa305cd38a1ed2d016ecef776162a9a
github username <github email address>
2019-03-25 17:41:03 -0400

    ruby version

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,7 +7,6 @@ class SessionsController < ApplicationController
 
   def create
     @user = User.find_by(name: params[:username])
-    byebug
 
     if @user && @user.authenticate(params[:password])
       session[:user_id] = @user.id
