4af22df1f0b4e4622a53aa0b64bcce0fa5fa80cd
github username <github email address>
2019-03-25 15:55:53 -0400

    user_mvp

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,6 +2,30 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
+    render :show
+  end
+
+  def edit
+    @user = User.find(params[:id])
+    render :edit
+  end
+
+  def update
+    @user = User.find(params[:id])
+    @user.update(user_params)
+    redirect_to user_path(@user.id)
+  end
+
+  def destroy
+    @user = User.find(params[:id])
+    @user.destroy
+    redirect_to "/"
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :address, :city, :state, :zip_code, :dob)
   end
 
 end
