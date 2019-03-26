793d7c4cbd7262def6f1eb6ee0dbf157aee26d26
github username <github email address>
2019-03-26 11:32:15 -0400

    added user validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,12 @@ class User < ApplicationRecord
   has_many :walkee_appointments, :class_name => 'Appointment', through: :dogs
   after_initialize :init
 
+  validates :name, :address, :city, :state, :zip_code, :dob, :password_digest, presence: true
+  validates :name, uniqueness: true
+  # validates :name, { :length => { in: 3..30}}
+  validates :password, length: { in: 6..20 }
+  validates :zip_code, { :length => { :is => 5 } }
+
   def init
     self.token_balance ||= 0
   end
