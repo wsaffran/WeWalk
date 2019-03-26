822cc2b487e7aa0eb7c54c4ff61ea014c3925394
github username <github email address>
2019-03-26 12:00:39 -0400

    tweaks

--- app/models/dog.rb
+++ app/models/dog.rb
@@ -2,6 +2,6 @@ class Dog < ApplicationRecord
   has_many :appointments
   belongs_to :user
 
-  validates :name, :breed, :age, :hypo_allergenic, :care_info, :presence => true
+  validates :name, :breed, :age, :hypo_allergenic, :care_info, :user_id, :presence => true
 
 end
